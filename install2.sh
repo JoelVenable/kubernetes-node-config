903d2acfe37cd9b27cb26f35208e816dfbf1df17
Joel Venable <joel.d.venable@gmail.com>
2020-08-05 22:35:28 +0000

    Updated script for version 1.18

--- install2.sh
+++ install2.sh
@@ -1,9 +1,12 @@
 . /etc/os-release
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O- | sudo apt-key add -
-apt-get update
 
-apt install -y cri-o-1.18
+
+
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing:/cri-o:/1.18/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:testing:cri-o:1.18.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:testing:cri-o:1.18/xUbuntu_18.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo apt-get install cri-o
 
 systemctl daemon-reload
 systemctl start crio
